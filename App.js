259674c5e29c4a848e954df4ec201b894a5ec327
Alex RH <alex.rosarioh@outlook.com>
2020-02-13 18:45:21 -0400

    First commit

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style = {styles.header}>
+        <Text>Icon</Text>
+        <Text>Logo</Text>
+        <Text>Icon</Text>
+      </View>
+      <View style = {styles.profileCard}>
+        <Text>Lorem ipsum blah blah blah</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F0F0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icons: {
+    tintColor: '#C5C5C5'
+  },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    height: '5%',
+    textAlign: 'center',
+    justifyContent: 'space-between'
+  },
+  profileCard: {
+    borderColor: '#C5C5C5',
+    borderSize: 1,
+    borderBottomEndRadius: 2,
+  }
+
+});
